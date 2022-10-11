daa35060826e5f17e5cc117b21e5053a95eff3da
tigr322 <70834080+tigr322@users.noreply.github.com>
2022-10-11 12:03:37 +0300

    Update javascript.js

--- zadan5/javascript.js
+++ zadan5/javascript.js
@@ -1,1 +1,24 @@
 
+function Zenakol() {
+    let price = document.getElementsByName("zena");
+    let count = document.getElementsByName("kol");
+    let result = document.getElementById("result");
+    let toPrice = zena[0].value;
+    let toCount = kol[0].value;
+    let z = toCount.match(/^\d+$/);
+    let k = toPrice.match(/^\d+$/);
+    if(z !== null && k !== null) {
+    result.innerHTML = parseInt(price[0].value) * parseInt(count[0].value);
+    }
+  return false;
+  }
+
+ function Done() {
+    alert("All DONE!");
+  }
+  
+  window.addEventListener("DOMContentLoaded", function (event) {
+    console.log("DOM fully loaded and parsed");
+    let b = document.getElementById("calculateResult");
+    b.addEventListener("click", Done);
+  });
