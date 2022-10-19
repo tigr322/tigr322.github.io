36fe0d13f04e44ae983fcdaf14b6f049fc9434c3
tigr322 <70834080+tigr322@users.noreply.github.com>
2022-10-19 18:56:26 +0300

    Update javascript.js

--- zadan6/javascript.js
+++ zadan6/javascript.js
@@ -1,18 +1,18 @@
 window.addEventListener('DOMContentLoaded', function (event) {
-  const PRICE = 400;
+  const PRICE_1 = 500;
   const PRICE_2 = 1000;
   const PRICE_3 = 2000;
-  const OVERPRICE = 10000;
-  const CAR = 100;
-  const CAR_2 = 150;
-  const CAR_3 = 200;
+  const EXTRA_PRICE = 10000;
+  const RADIO_1 = 100;
+  const RADIO_2 = 150;
+  const RADIO_3 = 200;
   const CHECKBOX = 3000;
-  let price = PRICE;
-  let overPrice = OVER_PRICE;
-  let cars = document.getElementById("mycars"); 
+  let price = PRICE_1;
+  let extraPrice = EXTRA_PRICE;
+  let radios = document.getElementById("myradios"); 
   let s = document.getElementsByName("myselect");
   let checks = document.getElementById("mycheck");
-  let c = document.querySelectorAll(".pis input[type=car]");
+  let r = document.querySelectorAll(".pis input[type=radio]");
   let checkbox = document.getElementById("check");
   let result = document.getElementById("result");
   let count = document.getElementsByName("count");
@@ -22,59 +22,59 @@ window.addEventListener('DOMContentLoaded', function (event) {
     let select = event.target;
     
     if (select.value === "1") {
-      cars.classList.add("d-none");
+      radios.classList.add("d-none");
       checks.classList.add("d-none");
-      overPrice = OVER_PRICE;
-      price = PRICE;
+      extraPrice = EXTRA_PRICE;
+      price = PRICE_1;
 
     }
     if(select.value === "2") {
-      cars.classList.remove("d-none");
+      radios.classList.remove("d-none");
       checks.classList.add("d-none");
       document.getElementById("radio1").checked = true;
-      overPrice =  OVER_PRICE;
+      extraPrice = EXTRA_PRICE;
       price = PRICE_2;
     }
     if(select.value === "3"){
-      cars.classList.add("d-none");
+      radios.classList.add("d-none");
       checks.classList.remove("d-none");
-      overPrice = OVER_PRICE;
+      extraPrice = EXTRA_PRICE;
       price = PRICE_3;
       document.getElementById("check").checked = false;
     }
   });
 
-c.forEach(function (currentCar) {
-    currentCar.addEventListener("change", function (event) {      
-        let c = event.target;
-        if (c.value === "c1") {      
-            overPrice = CAR;
-            console.log(c.value);
+r.forEach(function (currentRadio) {
+    currentRadio.addEventListener("change", function (event) {      
+        let r = event.target;
+        if (r.value === "c1") {      
+            extraPrice = RADIO_1;
+            console.log(r.value);
         }
-        if (c.value === "c2") {
-            overPrice = CAR_2;
-            console.log(c.value);
+        if (r.value === "c2") {
+            extraPrice = RADIO_2;
+            console.log(r.value);
         }
-        if (c.value === "c3") {
-            overPrice = CAR_3;
-            console.log(c.value);
+        if (r.value === "c3") {
+            extraPrice = RADIO_3;
+            console.log(r.value);
         }
     });
 });
 
 checkbox.addEventListener("change", function () {
   if (checkbox.checked) {
-      overPrice = CHECKBOX;
+      extraPrice = CHECKBOX;
   } else {
-      overPrice = OVER_PRICE;
+      extraPrice = EXTRA_PRICE;
   }
 });
 
 calc.addEventListener("change", function () {
   let toCount = count[0].value;
-  let f = toCount.match(/^\d+$/);
-  if(f!== null){
-    result.innerHTML=(price + overPrice )*parseInt(count[0].value);
+  let flag1488 = toCount.match(/^\d+$/);
+  if(flag1488 !== null){
+    result.innerHTML=(price + extraPrice)*parseInt(count[0].value);
   }
   else{
       result.innerHTML = "Введите число в поле - количество !";
